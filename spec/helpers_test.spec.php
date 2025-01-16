380e35a675d56cff60ff8cdbaf5e945aa11d7e94
william-man <william.man@dxw.com>
2025-01-16 16:50:18 +0000

    updated helpers_test.spec.php

--- spec/helpers_test.spec.php
+++ spec/helpers_test.spec.php
@@ -1,27 +1,30 @@
 <?php
 
-describe("Helper_Test", function(){
+describe(\Dxw\Iguana\Theme\Helpers::class, function(){
     beforeEach(function() {
         $this->helpers = new \Dxw\Iguana\Theme\Helpers();
     });
+    afterEach(function(){
+    });
+
     it("test register function", function(){
         $this->helpers->registerFunction('myFunc', function() {
             return 42;
         });
-        expect($this->helpers->myFunc())->toBe(42);
+        expect($this->helpers->__call('myFunc',[]))->toBe(42);
     });
 
     it("test function arguements", function(){
         $this->helpers->registerFunction('anotherFunc', function($a, $b) {
             return 42 + $a + $b;
         });
-        expect($this->helpers->anotherFunc(1,5))->toBe(48);
+        expect($this->helpers->__call('anotherFunc',[1,5]))->toBe(48);
     });
 
     it("test register", function(){
         expect($this->helpers)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
+        
         expect(function_exists('h'))->toBeFalsy();
-
         $this->helpers->register();
         expect(function_exists('h'))->toBeTruthy();
     });
